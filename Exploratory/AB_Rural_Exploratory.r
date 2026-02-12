f64bc40d0877d15f7f2402f3692d884d428c8cd9
Mike Williams <mpw144@gmail.com>
2026-02-12 11:44:29 -0500

    set up new R file for this analysis -- exploratory phase

---
+++ Exploratory/AB_Rural_Exploratory.r
@@ -0,0 +1,23 @@
+#libraries
+library(tidyverse)
+library(dplyr)
+library(knitr)
+library(tableone)
+library(ggplot2)
+options(scipen=999)
+library(mediation)
+library(MASS)
+library(CMAverse)
+library(mice)
+library(stringr)
+library(psych)
+library(sjPlot)
+library(gtools)
+library(drc)
+library(lm.beta)
+library(apaTables)
+
+
+#set working directory
+setwd('/Users/michaelwilliams/Documents/MGH T32/Research/AB/Analysis/AB_Rural/Exploratory')
+getwd()
